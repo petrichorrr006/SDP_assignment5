bcecf132d972cbe6f65e35e44f2e0465dbea58bb
andronousss <raven24112006@gmail.com>
2025-10-19 20:24:34 +0500

    Create ChatServer.java

---
+++ ChatServer.java
@@ -0,0 +1,11 @@
+package SDP_assignment5;
+
+public class ChatServer {
+    public void connect() {
+        System.out.println("Connected to chat server...");
+    }
+
+    public void send(String msg) {
+        System.out.println("Sending message: " + msg);
+    }
+}
